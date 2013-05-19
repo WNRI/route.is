57ed7065b827cbe8063a24559b9c102f285ab480
Guttorm Flatabø <post@guttormflatabo.com>
2013-05-19 21:17:02 +0000

    Add path to applycange.sh

--- osmdiffs/applychange.sh
+++ osmdiffs/applychange.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 # Waymarked Trails update script
 # Make change file, will only fetch one day from current state
+cd /srv/www/route.is/osmdiffs
 osmosis --rri --simc --wxc
 # Import changes
 echo "truncate node_changeset; truncate way_changeset; truncate relation_changeset;" | psql -d norway
-osgende-import -d norway -m 10000000 -t /tmp change.osc
+/usr/local/bin/osgende-import -d norway -m 10000000 -t /tmp change.osc
 cd ../db/src/
 python makedb.py -j 4 -d norway hiking update
 # Write timestamp
